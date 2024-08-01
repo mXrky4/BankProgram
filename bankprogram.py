fef8de9be0a1492d27784426ca81857c8a798cb7
mXrky4 <ryanjoelraymond@gmail.com>
2024-08-02 03:09:28 +0800

    Final bugfixing and aesthetic

--- bankprogram.py
+++ bankprogram.py
@@ -1,58 +1,74 @@
-def show_balance():
+def show_balance(balance):
     print(".........................................")
     print(f"Your balance is RM{balance:.2f}")
-    print(".........................................")
+    
     
 
 def deposit():
     print(".........................................")
     amount = float(input("Enter an amount to be deposited: "))
-    print(".........................................")
+    
     if amount < 0:
+        
         print("That's not a valid amount")
         print(".........................................")
+        
         return 0  
     else:
         return amount
     
 
-def withdrawal():
+def withdrawal(balance):
     print(".........................................")
     amount = float(input("Enter an amount to be withdrawn: "))
-    print(".........................................")
+    
+    
     if amount > balance:
-        print("You have insufficient funds")
         print(".........................................")
+        print("You have insufficient funds")
+        
         return 0
     elif amount < 0:
-        print("Amount must not be less than 0")
         print(".........................................")
+        print("Amount must not be less than 0")
+        
         return 0
     else:
         return amount
 
-balance=0
-is_running = True
 
-while is_running:
-    print("Bank Program")
-    print("1.Check balance")
-    print("2.Deposit")
-    print("3.Withdraw")
-    print("4.Exit")
+def main():
 
-    choice= input("Enter your choice (1-4):")
+    balance=0
+    is_running = True
 
-    if choice == "1":
-        show_balance()
-    elif choice == "2":
-        balance += deposit()
-    elif choice == "3":
-        balance -= withdrawal()
-    elif choice == "4":
-        is_running = False
-    else:
-        print("That is an invalid choice")
+    while is_running:
         print(".........................................")
-print(".........................................")
-print("Thank you!! Have a nice day!")
+        print("Bank Program")
+        print("1.Check balance")
+        print("2.Deposit")
+        print("3.Withdraw")
+        print("4.Exit")
+        print(".........................................")
+        choice= input("Enter your choice (1-4):")
+        
+
+        if choice == "1":
+            show_balance(balance)
+        elif choice == "2":
+            balance += deposit()
+        elif choice == "3":
+            balance -= withdrawal(balance)
+        elif choice == "4":
+            is_running = False
+        else:
+            print(".........................................")
+            print("That is an invalid choice")
+            
+    print(".........................................")
+    print("Thank you!! Have a nice day!")
+    print(".........................................")
+    
+
+if __name__=='__main__':
+    main()
